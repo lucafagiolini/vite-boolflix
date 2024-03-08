d89d51461cc7ee7af5f8508b304f7472e24ea520
Luca <lucafagiolini@live.it>
2024-03-08 16:08:09 +0100

    added function to call in the arrayu from the api

--- src/components/AppSearchBar.vue
+++ src/components/AppSearchBar.vue
@@ -6,9 +6,24 @@ export default {
     name: 'AppSearchBar',
     data() {
         return {
-            store, 
+            store,
+            searchQuery: '',
         }
     },
+
+    methods: {
+        searchMovies() {
+            let formattedQuery = this.searchQuery.split(' ').join('+');
+            axios.get(`https://api.themoviedb.org/3/search/movie?api_key=c7bfad605bd2ec8fb7088c733fe64d61&query=${formattedQuery}`)
+                .then(response => {
+                    this.store.movies = response.data.results;
+                    console.log(this.store.movies);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    }
 }
 
 </script>
@@ -22,7 +37,7 @@ export default {
 
 <template>
     <div class="container">
-        <input type="text"  placeholder="Search for a movie" />
+        <input type="text" v-model="this.searchQuery"  placeholder="Search for a movie" />
         <button @click="searchMovies">Search</button>
     </div>
 </template>
