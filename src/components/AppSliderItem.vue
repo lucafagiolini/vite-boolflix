0afa782ae5e2ec5e545df3cd8d1449393dc6453c
Luca <lucafagiolini@live.it>
2024-03-10 19:50:47 +0100

    milestone 4 checkpoint

--- src/components/AppSliderItem.vue
+++ src/components/AppSliderItem.vue
@@ -18,18 +18,19 @@ export default {
 
 <template>
 
-    <li class="card">
+    <li class="card position-relative">
         <img class="w-100" tabindex="0" :src="movie && movie.poster_path ? 'https://image.tmdb.org/t/p/w342' + movie.poster_path : 'https://www.innerintegratori.it/wp-content/uploads/2021/06/placeholder-image.png'" alt="">
-
-        <h5>{{ movie.title }}</h5>
-   
-        <h5> {{ movie.original_title }}</h5>
-
-        <img class="flag" :src="'/src/assets/flags/' + movie.original_language + '.png'" alt="">  
-
-        <span> {{ movie.original_language }}</span> 
+        <div id="movie_details" class="position-absolute h-100 w-100">
+            <h5>{{ movie.title }}</h5>
+       
+            <h5> {{ movie.original_title }}</h5>
     
-        <span> {{ movie.vote_average }} </span>
+            <img class="flag" :src="'/src/assets/flags/' + movie.original_language + '.png'" alt="">  
+    
+            <span> {{ movie.original_language }}</span> 
+        
+            <span> {{ movie.vote_average }} </span>
+        </div>
     </li>
 
 </template>
@@ -37,7 +38,7 @@ export default {
 
 
 
-<style>
+<style lang="scss">
 .card {
     width: 300px;
     object-fit: cover;
@@ -50,6 +51,26 @@ export default {
     .flag {
         width: 20px;
         object-fit: cover;
+    }
+
+    #movie_details {
+        display: none;
+        
+    }
+
+    &:hover {
+
+        .card {
+            transform: scale(1.1);
+            transition: transform 0.5s;
+            
+        }
+
+        #movie_details {
+            display: flex;
+            flex-direction: column;
+            background-color: rgba(0, 0, 0, 0.887);
+        }
     }
 
 }
